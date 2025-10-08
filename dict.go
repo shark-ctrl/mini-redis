398e1bd3f4585b9d44bbc218eef521038fd51501
sharkchili <1946955309@qq.com>
2025-10-08 22:54:03 +0800

    feat(dict): 增加字典添加键值对功能并优化数据结构定义
    - 添加 DICT_OK 和 DICT_ERR 常量用于返回状态标识
    - 修改 dictht 结构体字段类型：table由 **dictEntry 改为 *[]*dictEntry， sizemask 由 uint64 改为 int
    - 修改 dict 结构体字段名：t 改为 dType，ht 类型由 [2]dictht 改为 *[2]dictht
    - 新增 dictAdd 和 dictAddRaw 函数实现键值对的添加逻辑
    - 新增 dictIsRehashing 函数判断是否处于渐进式哈希状态
    - 新增 _dictKeyIndex 函数用于计算 key 的存储索引并检查 key 是否已存在

--- dict.go
+++ dict.go
@@ -1,6 +1,8 @@
 package main
 
 const dict_hash_function_seed = 5381
+const DICT_OK = 0
+const DICT_ERR = 1
 
 /**
  * 字典键值对定义
@@ -15,9 +17,9 @@ type dictEntry struct {
  * 字典哈希表定义
  */
 type dictht struct {
-	table    **dictEntry
+	table    *[]*dictEntry
 	size     uint64
-	sizemask uint64
+	sizemask int
 	used     uint64
 }
 
@@ -25,9 +27,9 @@ type dictht struct {
  * 字典核心数据结构定义
  */
 type dict struct {
-	t         *dictType
+	dType     *dictType
 	privdata  *interface{}
-	ht        [2]dictht
+	ht        *[2]dictht
 	rehashidx int64
 	iterators int
 }
@@ -53,4 +55,73 @@ func dictReplace(d map[string]*robj, key *robj, val *robj) bool {
 		d[k] = val
 		return true
 	}
+}
+
+func dictAdd(d *dict, k *string, v *interface{}) int {
+	entry := dictAddRaw(d, k)
+	if entry == nil {
+		return DICT_ERR
+	}
+	entry.val = v
+	return DICT_OK
+}
+
+func dictAddRaw(d *dict, k *string) *dictEntry {
+	//todo 判断是否需要渐进式哈希
+
+	index := _dictKeyIndex(d, k)
+	//检查key是否存在
+	if index == -1 {
+		return nil
+
+	}
+	//根据渐进式哈希表确定table
+	var ht dictht
+	if dictIsRehashing(d) {
+		ht = d.ht[1]
+	} else {
+		ht = d.ht[0]
+	}
+
+	//将key设置到对应table的拉链上，并维护必要的信息
+	i := interface{}(k)
+	entry := &dictEntry{key: &i}
+	entry.next = (*(ht.table))[index]
+	ht.used++
+
+	return entry
+}
+
+func dictIsRehashing(d *dict) bool {
+	return d.rehashidx != -1
+}
+
+func _dictKeyIndex(d *dict, key *string) int {
+
+	var idx int
+	//todo 判断是否需要扩容
+
+	//计算索引位置
+
+	h := d.dType.hashFunction(key)
+
+	//基于索引定位key
+	for i := 0; i < 2; i++ {
+		idx := h & d.ht[i].sizemask
+		he := (*(d.ht[i].table))[idx]
+
+		for he != nil {
+			if d.dType.keyCompare(nil, key, key) {
+				return -1
+			}
+			he = he.next
+		}
+
+		// 如果正在rehash，则检查ht[1]
+		if dictIsRehashing(d) {
+			break
+		}
+	}
+
+	return idx
 }
